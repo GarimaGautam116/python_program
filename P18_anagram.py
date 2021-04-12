65d71d2efa6118499b4e2128938cf9f7b3fda9c9
Garima Gautam <80679679+GarimaGautam116@users.noreply.github.com>
2021-04-12 20:38:45 +0530

    Add files via upload
    Given string is anagram or not

---
+++ P18_anagram.py
@@ -0,0 +1,10 @@
+def check(s1, s2):
+     
+    if(sorted(s1)== sorted(s2)):
+        print("The strings are anagrams.")
+    else :
+        print("The strings aren't anagrams.")        
+          
+s1 =input("Enter the string1")
+s2 =input("Enter the string2")
+check(s1, s2)
